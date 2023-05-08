fdee4bd0b333ca2023cab1214c8898ac10e364fc
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-05-08 19:56:49 +0300

    add scroll plug

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 window.addEventListener('scroll', e => {
-    document.body.style.cssText = `--scrollTop: ${this.scrollY}px`
+    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
 });
 
 // WebGLSampler.registerPlugin(ScrollTrigger, ScrollSmoother);
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.container',
+})
